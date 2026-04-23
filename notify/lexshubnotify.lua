23c795f92f756331c5bb330f698f906326a5dbf1
nyzxhub-rblx <nyzxhub@gmail.com>
2026-04-23 18:03:05 +0700

    Update lexshubnotify.lua

--- notify/lexshubnotify.lua
+++ notify/lexshubnotify.lua
@@ -7,24 +7,25 @@ return function()
     local PlayerGui = player:WaitForChild("PlayerGui")
 
     local Lib = {}
+    local NotifCount = 0
 
     function Lib:MakeNotify(options)
         local Title = options.Title or "Notification"
         local Content = options.Content or ""
         local Delay = options.Delay or 3
         local Icon = options.Icon or ""
-        
-        -- ICON HANDLE (lucide:)
+
+        -- lucide convert
         if Icon:find("lucide:") then
-            local name = Icon:gsub("lucide:", "")
-            
             local icons = {
                 check = "rbxassetid://7733658504",
                 x = "rbxassetid://7733658504",
                 alert = "rbxassetid://7733658504"
             }
-            Icon = icons[name] or ""
+            Icon = icons[Icon:gsub("lucide:", "")] or ""
         end
+
+        NotifCount += 1
 
         local ScreenGui = Instance.new("ScreenGui")
         ScreenGui.Parent = PlayerGui
@@ -32,7 +33,10 @@ return function()
 
         local Frame = Instance.new("Frame")
         Frame.Size = UDim2.new(0, 300, 0, 75)
-        Frame.Position = UDim2.new(1, 50, 0.5, -37)
+
+        -- 🔥 POSISI KANAN BAWAH + STACK
+        Frame.Position = UDim2.new(1, 320, 1, -100 - ((NotifCount-1) * 85))
+
         Frame.BackgroundColor3 = Color3.fromRGB(10,10,10)
         Frame.Parent = ScreenGui
 
@@ -77,37 +81,39 @@ return function()
         ContentLabel.TextXAlignment = Enum.TextXAlignment.Left
         ContentLabel.Parent = Frame
 
+        -- BAR
         local BarBG = Instance.new("Frame")
         BarBG.Size = UDim2.new(1, 0, 0, 3)
         BarBG.Position = UDim2.new(0, 0, 1, -3)
         BarBG.BackgroundColor3 = Color3.fromRGB(30,30,30)
-        BarBG.BorderSizePixel = 0
         BarBG.Parent = Frame
 
         local Bar = Instance.new("Frame")
         Bar.Size = UDim2.new(1, 0, 1, 0)
-        Bar.BorderSizePixel = 0
         Bar.Parent = BarBG
 
         local BarGradient = Instance.new("UIGradient", Bar)
         BarGradient.Color = Gradient.Color
 
+        -- MASUK
         TweenService:Create(Frame, TweenInfo.new(0.3), {
-            Position = UDim2.new(1, -320, 0.5, -37)
+            Position = UDim2.new(1, -320, 1, -100 - ((NotifCount-1) * 85))
         }):Play()
 
+        -- PROGRESS
         TweenService:Create(Bar, TweenInfo.new(Delay), {
             Size = UDim2.new(0, 0, 1, 0)
         }):Play()
 
+        -- HAPUS
         task.delay(Delay, function()
             TweenService:Create(Frame, TweenInfo.new(0.3), {
-                Position = UDim2.new(1, 50, 0.5, -37),
-                BackgroundTransparency = 1
+                Position = UDim2.new(1, 320, 1, -100)
             }):Play()
 
             task.wait(0.3)
             ScreenGui:Destroy()
+            NotifCount -= 1
         end)
     end
 
